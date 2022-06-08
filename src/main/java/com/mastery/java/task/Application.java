aab76438c2223449abb065c7d6bfe25771078f82
korzunEvgeniy <korzun-ferrero@ya.ru>
2022-06-08 21:53:52 +0300

    Correct configuration

--- src/main/java/com/mastery/java/task/Application.java
+++ src/main/java/com/mastery/java/task/Application.java
@@ -2,10 +2,11 @@ package com.mastery.java.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.mastery.java.task.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.mastery.java.task")
-public class AppConfiguration {
-}

3c4f582537d613691f328dca56b88adf5ff2bbbc
korzunEvgeniy <korzun-ferrero@ya.ru>
2022-06-09 06:01:15 +0300

    Add remote data base using ElephantSQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,4 +11,10 @@ VALUES
     ('Irina', 'Kravec', 'designer', (SELECT id FROM gender WHERE name = 'FEMALE'), '1990-06-05'),
     ('Polina', 'Filipova', 'engineer', (SELECT id FROM gender WHERE name = 'FEMALE'), '1991-04-23'),
     ('Kris', 'DeBurg', 'developer', (SELECT id FROM gender WHERE name = 'MALE'), '1980-05-20'),
-    ('Lara', 'Fabian', 'developer', (SELECT id FROM gender WHERE name = 'FEMALE'), '1990-07-20');
+    ('Lara', 'Fabian', 'developer', (SELECT id FROM gender WHERE name = 'FEMALE'), '1990-07-20'),
+    ('James', 'Bond', 'engineer', (SELECT id FROM gender WHERE name = 'MALE'), '1987-04-12'),
+    ('Jonny', 'Depp', 'developer', (SELECT id FROM gender WHERE name = 'MALE'), '1978-06-30'),
+    ('Sveta', 'Shein', 'designer', (SELECT id FROM gender WHERE name = 'FEMALE'), '1995-05-25'),
+    ('Alex', 'Bolduin', 'developer', (SELECT id FROM gender WHERE name = 'MALE'), '1980-12-27'),
+    ('Bob', 'Marley', 'engineer', (SELECT id FROM gender WHERE name = 'MALE'), '1985-04-05'),
+    ('Evgeniy', 'Korzun', 'developer', (SELECT id FROM gender WHERE name = 'MALE'), '1987-06-24');
